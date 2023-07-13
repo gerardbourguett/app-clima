6bff710d627da9150e46a01565ffc296203fcf0a
Gerard Bourguett <gerard.bourguett@gmail.com>
2023-07-12 21:18:18 -0400

    Cambios bootstrap

--- myapp/views.py
+++ myapp/views.py
@@ -4,18 +4,24 @@ import requests
 # Create your views here.
 
 
-def clima(ciudad):
-    url = "https://weatherapi-com.p.rapidapi.com/current.json"
-    querystring = {"q": ciudad}
+def obtener_clima(ciudad, dias):
+    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
+    querystring = {"q": ciudad, "days": dias}
     headers = {
         "X-RapidAPI-Key": "7b81b72228mshcf22ecf1a65c3e3p11720djsn8e421da9f90a",
         "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
     }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    return response.json()
+
+    try:
+        response = requests.get(url, headers=headers, params=querystring)
+        clima = response.json()
+        return clima
+    except requests.exceptions.RequestException as e:
+        print("Error al obtener el clima:", e)
+        return None
 
 
 def index(request):
     ciudad = request.GET.get("ciudad")
-    datos_clima = clima(ciudad)
-    return render(request, "index.html", {"clima": datos_clima})
+    clima = obtener_clima(ciudad, dias="5")
+    return render(request, "index.html", {"clima": clima})
